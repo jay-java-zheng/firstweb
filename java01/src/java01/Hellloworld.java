0163718e5842ad06c4b25f8ccb9fa2a8ab33fabc
jay <jay@192.168.0.106>
2018-02-04 22:23:26 +0800

    lll

---
+++ java01/src/java01/Hellloworld.java
@@ -0,0 +1,20 @@
+package java01;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author wangzheng
+ *
+ */
+public class Hellloworld {
+	String name;
+	int age;
+	public static void main(String[] args) {
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
